ce9390392343de03957da063294a6007958fecfe
Edward Baitsewe <edward@monatemedia.com>
2024-11-29 01:52:06 +0200

    Commit Changes

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+<h1>Welcome to the Dealership</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente voluptatem voluptate veniam, nesciunt odio
+    reiciendis? Veniam, laborum, natus doloremque harum suscipit pariatur fugit nobis odit numquam deserunt voluptatum
+    possimus porro.</p>
