134b805930da2f2af498a99282a23b322f8b4b5d
Edward Baitsewe <edward@monatemedia.com>
2024-12-20 17:33:24 +0200

    Make basic layouts into components

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::view('about', 'about')->name('about');
+Route::get('/signup', [SignupController::class, 'create'])->name('signup');
+
+Route::get('/login', [LoginController::class, 'create'])->name('login');
 
