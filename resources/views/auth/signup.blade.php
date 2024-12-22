444f57bc930c6a9d5adc883d9acaa8e9a19cd3d3
Edward Baitsewe <edward@monatemedia.com>
2024-12-22 02:37:57 +0200

    Create Guest Layout

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,53 +1,32 @@
-<x-base-layout title="Sign Up" bodyClass="page-signup">
-    <main>
-        <div class="container-small page-login">
-            <div class="flex" style="gap: 5rem">
-                <div class="auth-page-form">
-                    <div class="text-center">
-                        <a href="/">
-                            <img src="/img/logoipsum-265.svg" alt="" />
-                        </a>
-                    </div>
-                    <h1 class="auth-page-title">Signup</h1>
+<x-guest-layout title="Sign Up" bodyClass="page-signup">
 
-                    <form action="" method="post">
-                        <div class="form-group">
-                            <input type="email" placeholder="Your Email" />
-                        </div>
-                        <div class="form-group">
-                            <input type="password" placeholder="Your Password" />
-                        </div>
-                        <div class="form-group">
-                            <input type="password" placeholder="Repeat Password" />
-                        </div>
-                        <hr />
-                        <div class="form-group">
-                            <input type="text" placeholder="First Name" />
-                        </div>
-                        <div class="form-group">
-                            <input type="text" placeholder="Last Name" />
-                        </div>
-                        <div class="form-group">
-                            <input type="text" placeholder="Phone" />
-                        </div>
-                        <button class="btn btn-primary btn-login w-full">Register</button>
-
-                        <div class="grid grid-cols-2 gap-1 social-auth-buttons">
-                            <!-- Google Button -->
-                            <x-google-button />
-                            <!-- FaceBook Button -->
-                            <x-fb-button />
-                        </div>
-                        <div class="login-text-dont-have-account">
-                            Already have an account? -
-                            <a href="/login.html"> Click here to login </a>
-                        </div>
-                    </form>
-                </div>
-                <div class="auth-page-image">
-                    <img src="/img/car-png-39071.png" alt="" class="img-responsive" />
-                </div>
-            </div>
+    <h1 class="auth-page-title">Signup</h1>
+    <form action="" method="post">
+        <div class="form-group">
+            <input type="email" placeholder="Your Email" />
         </div>
-    </main>
-</x-base-layout>
+        <div class="form-group">
+            <input type="password" placeholder="Your Password" />
+        </div>
+        <div class="form-group">
+            <input type="password" placeholder="Repeat Password" />
+        </div>
+        <hr />
+        <div class="form-group">
+            <input type="text" placeholder="First Name" />
+        </div>
+        <div class="form-group">
+            <input type="text" placeholder="Last Name" />
+        </div>
+        <div class="form-group">
+            <input type="text" placeholder="Phone" />
+        </div>
+        <button class="btn btn-primary btn-login w-full">Register</button>
+    </form>
+
+    <x-slot:footerLink>
+        Already have an account? -
+        <a href="/login"> Click here to login </a>
+    </x-slot:footerLink>
+
+</x-guest-layout>
