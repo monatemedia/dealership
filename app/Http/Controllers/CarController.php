2c98b31665989b476b60898cb7c9e3374c51d193
Edward Baitsewe <edward@monatemedia.com>
2025-04-12 06:56:16 +0200

    Customize Validation Messages and Attributes

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCarRequest;
 use App\Models\Car;
 use App\Models\User;
 use App\Rules\Phone;
@@ -47,29 +48,10 @@ class CarController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCarRequest $request)
     {
         // Get request data
-        $data = $request->validate([
-            'manufacturer_id' => 'required',
-            'model_id' => 'required',
-            'year' => ['required', 'integer', 'min:1900', 'max:' . date('Y')],
-            'price' => 'required|integer|min:0',
-            'vin' => 'required|string|size:17',
-            'mileage' => 'required|integer|min:0',
-            'car_type_id' => 'required|exists:car_types,id',
-            'fuel_type_id' => 'required|exists:fuel_types,id',
-            'city_id' => 'required|exists:cities,id',
-            'address' => 'required|string',
-            'phone' => 'required|string|min:10',
-            'description' => 'nullable|string',
-            'published_at' => 'nullable|string',
-            'features' => 'array',
-            'features.*' => 'string',
-            'images' => 'array',
-            'images.*' => File::image()
-                ->max(2048)
-        ]);
+        $data = $request->validated();
 
         $featuresData = $data['features']; // Get features data
         $images = $request->file('images') ?: []; // Get images data
