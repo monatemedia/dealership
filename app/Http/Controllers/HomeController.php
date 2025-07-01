eb4e835297ed5ac4edab0c534c0d19596f1253b2
Edward Baitsewe <edward@monatemedia.com>
2025-07-02 01:45:08 +0200

    Implement Caching on Home Page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\Car;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        // Select latest published 30 cars and sort them by published_at date
-        $cars = Car::with(['city', 'carType', 'fuelType', 'manufacturer', 'model', 'primaryImage', 'favouredUsers']) // Eager load the relationships
-            ->where('published_at', '<', now()) // Only show cars that are published
-            ->orderBy('published_at', 'desc') // Order by the published_at date
-            ->limit(30) // Limit the results to 30
-            ->get(); // Get the results
+        $cars = Cache::remember('home-cars', 60, function () {
+            return Car::where('published_at', '<', now())
+                ->with(['primaryImage', 'city', 'carType', 'fuelType', 'manufacturer', 'model', 'favouredUsers'])
+                ->orderBy('published_at', 'desc')
+                ->limit(30)
+                ->get();
+        });
 
         // Return the view with the cars
         return view(
