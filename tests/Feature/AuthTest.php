cbc126775cd2dcd04474355dcac4e557f5c88e95
Edward Baitsewe <edward@monatemedia.com>
2025-07-03 16:58:06 +0200

    Shorter Syntax of Writing Tests

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -2,6 +2,7 @@
 
 // Test for login page
 it('returns success on login page', function () {
+    /** @var \Illuminate\Testing\TestResponse $response */
     $response = $this->get(route('login'));
 
     $response->assertStatus(200);
@@ -9,6 +10,7 @@ it('returns success on login page', function () {
 
 // Test for the signup page
 it('returns success on signup page', function () {
+    /** @var \Illuminate\Testing\TestResponse $response */
     $response = $this->get(route('signup'));
 
     $response->assertStatus(200);
@@ -16,6 +18,7 @@ it('returns success on signup page', function () {
 
 // Test for the password forgot page
 it('returns success on forgot password page', function () {
+    /** @var \Illuminate\Testing\TestResponse $response */
     $response = $this->get(route('password.request'));
 
     $response->assertStatus(200);
@@ -25,6 +28,7 @@ it('returns success on forgot password page', function () {
 it('redirects to login page, when accessing car create page as guest user', function () {
     /** @var \Illuminate\Testing\TestResponse $response */
     $response = $this->get(route('car.create'));
+
     // Assert that the response is a redirect to the login route
     $response->assertRedirectToRoute('login');
     // Assert that the response status is 302 (redirect)
