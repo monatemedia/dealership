86b6862483b9735b1e1a41adc39e23679a29ba88
Edward Baitsewe <edward@monatemedia.com>
2025-07-07 02:52:24 +0200

    Test Signup Page

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -26,7 +26,7 @@ it('returns success on login page', function () {
         ->assertSee(route('login.oauth', ['provider' => 'facebook']), false);
 });
 
-// Test for login with correct credentials
+// Test for login with incorrect credentials
 it('should not be possible to login with incorrect credentials', function () {
     // Create a user using the factory
     \App\Models\User::factory()->create([
@@ -78,7 +78,104 @@ it('returns success on signup page', function () {
     /** @var \Illuminate\Testing\TestResponse $response */
     $response = $this->get(route('signup'));
 
-    $response->assertStatus(200);
+    // Check if the response status is 200
+    $response->assertStatus(200)
+        // Check if the response contains the text 'Signup'
+        ->assertSee('Signup')
+        // Check if the response contains the text 'Click here to login'
+        ->assertSee('Click here to login')
+        // Check if the response contains the text 'Google'
+        ->assertSee('Google')
+        // Check if the response contains the text 'Facebook'
+        ->assertSee('Facebook')
+        // Check if the response contains a link to the login page
+        ->assertSee('<a href="' . route('login') . '"', false)
+        // Check if the response contains links to the OAuth login pages for Google and Facebook
+        ->assertSee(route('login.oauth', ['provider' => 'google']), false)
+        ->assertSee(route('login.oauth', ['provider' => 'facebook']), false);
+});
+
+// Test for signup with empty data
+it('should not be possible to signup with empty data', function () {
+
+    /** @var \Illuminate\Testing\TestResponse $response */
+    $response = $this->post(route('signup.store'), [
+        'name' => '', // Empty name to trigger validation error
+        'email' => '', // Empty email to trigger validation error
+        'phone' => '', // Empty phone number to trigger validation error
+        'password' => '', // Empty password to trigger validation error
+        'password_confirmation' => '', // Empty password confirmation to trigger validation error
+    ]);
+
+    // Assert that the response status is 302 (redirect)
+    $response->assertStatus(302)
+        // Assert that the session has an 'email' key
+        // ->assertSessionHasErrors(['email']);
+        ->assertInvalid(['name', 'email', 'phone', 'password']);
+});
+
+// Test for signup with incorrect password
+it('should not be possible to signup with incorrect password', function () {
+
+    /** @var \Illuminate\Testing\TestResponse $response */
+    $response = $this->post(route('signup.store'), [
+        'name' => 'Edward', // Valid name
+        'email' => 'edward@gmail.com', // Invalid email to trigger validation error
+        'phone' => '123',
+        'password' => '123456',
+        'password_confirmation' => '111111', // Password confirmation to trigger validation error
+    ]);
+
+    // Assert that the response status is 302 (redirect)
+    $response->assertStatus(302)
+        // Assert that the session has an 'email' key
+        // ->assertSessionHasErrors(['email']);
+        ->assertInvalid(['password']);
+});
+
+// Test for signup with existing email
+it('should not be possible to signup with an existing email', function () {
+
+    // Create a user using the factory
+    \App\Models\User::factory()->create([
+        'email' => 'edward@gmail.com'
+    ]);
+
+    /** @var \Illuminate\Testing\TestResponse $response */
+    $response = $this->post(route('signup.store'), [
+        'name' => 'Edward', // Valid name
+        'email' => 'edward@gmail.com', // Attempting to use an existing email
+        'phone' => '123',
+        'password' => '1asda523Aa.#', // Valid password
+        'password_confirmation' => '1asda523Aa.#',
+    ]);
+
+    // Assert that the response status is 302 (redirect)
+    $response->assertStatus(302)
+        // Assert that the session has an 'email' key
+        ->assertInvalid(['email']);
+});
+
+// Test for signup with correct credentials
+it('should be possible to signup with correct credentials', function () {
+
+    /** @var \Illuminate\Testing\TestResponse $response */
+    $response = $this->post(route('signup.store'), [
+        'name' => 'Edward',
+        'email' => 'edward@gmail.com',
+        'phone' => '123',
+        'password' => 'Dajhdga12312@#',
+        'password_confirmation' => 'Dajhdga12312@#',
+    ]);
+
+    // Dump the session data for debugging
+    // $response->ddSession();
+
+    // Assert that the response status is 302 (redirect)
+    $response->assertStatus(302)
+        // Assert that the user is redirected to the home page
+        ->assertRedirectToRoute('home')
+        ->assertSessionHas(['success']);
 });
 
 // Test for the password forgot page
