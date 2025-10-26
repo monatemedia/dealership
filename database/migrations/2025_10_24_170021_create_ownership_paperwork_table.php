51fa7651a302bc9b8722f18baf24a6a62f51cfb0
Edward Baitsewe <edward@monatemedia.com>
2025-10-26 03:51:39 +0200

    Fix missing timestamps in `create_ownerhip_paperwork_table` migration

--- database/migrations/2025_10_24_170021_create_ownership_paperwork_table.php
+++ database/migrations/2025_10_24_170021_create_ownership_paperwork_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->string('category'); // 'Ownership Checklist', 'Supporting Documents', etc.
+            $table->timestamps();
         });
     }
 
