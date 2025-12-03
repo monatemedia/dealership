132a6c6a8221bc2a7980b1e4ae78753bb64f3119
Edward Baitsewe <edward@monatemedia.com>
2025-12-03 12:21:02 +0200

    Testing CICD

--- resources/views/components/hero/home-slider.blade.php
+++ resources/views/components/hero/home-slider.blade.php
@@ -21,7 +21,7 @@
         </x-slot:image>
 
         <x-hero.heading level="h1">
-            {{ $categoryName }} in <br>
+           Best {{ $categoryName }} in <br>
             <strong>Mzansi</strong>
         </x-hero.heading>
 
